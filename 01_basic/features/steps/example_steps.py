e1b9d9ed625801affa2f6f85c49f3dd547ab6db6
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2019-05-14 13:50:51 +0200

    update example

--- 01_basic/features/steps/example_steps.py
+++ 01_basic/features/steps/example_steps.py
@@ -6,10 +6,10 @@ def step_impl(context):
 
 @when('we implement {number:d} tests')
 def step_impl(context, number):  # -- NOTE: number is converted into integer
-    assert number > 1 or number == 0
+    assert number > 0
     context.tests_count = number
 
 @then('behave will test them for us!')
 def step_impl(context):
     assert context.failed is False
-    assert context.tests_count >= 0
+    assert context.tests_count > 0
