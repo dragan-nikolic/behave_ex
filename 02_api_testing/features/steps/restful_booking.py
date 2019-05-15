cd481f1289f74dea4bff0deaff57dcf155157c35
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2019-05-15 17:14:04 +0200

    implement get test case :)

--- 02_api_testing/features/steps/restful_booking.py
+++ 02_api_testing/features/steps/restful_booking.py
@@ -1,10 +1,22 @@
 import requests
 
+class ResponseError(Exception):
+    pass
+
+class ValidationError(Exception):
+    pass
+
 def _url(path):
     return 'https://restful-booker.herokuapp.com' + path
 
-def get_bookings(firstname="", lastname="", checkin="", checkout=""):
+def get_booking_ids(
+        firstname="", 
+        lastname="", 
+        checkin="", 
+        checkout=""):
+
     payload = {}
+
     if firstname:
         payload['firstname'] = firstname
     if lastname:
@@ -15,15 +27,38 @@ def get_bookings(firstname="", lastname="", checkin="", checkout=""):
         payload['checkout'] = checkout
 
     if payload:
-        return requests.get(_url('/booking/'), params=payload)
+        response = requests.get(_url('/booking/'), params=payload)
     else:
-        return requests.get(_url('/booking/'))
+        response = requests.get(_url('/booking/'))
+
+    if response.status_code != 200:
+        raise ResponseError(
+            "Incorrect status code: {} ({})".format(
+                response.status_code,
+                response.reason))
+
+    return [x['bookingid'] for x in response.json()]
+
+def get_booking(id):
+    response = requests.get(_url("/booking/{}".format(id)))
+
+    if response.status_code != 200:
+        raise ResponseError(
+            "Incorrect status code: {} ({})".format(
+                response.status_code,
+                response.reason))
+
+    return response.json()
 
 @when(u'I request Mark\'s booking')
 def step_impl(context):
-    resp = get_bookings('Mark')
-    print(resp.json())
+    context.user = 'Mark'
+    context.booking_ids = get_booking_ids(context.user)
 
 @then(u'I should receive Mark\'s booking info')
 def step_impl(context):
-    raise NotImplementedError(u'STEP: Then I should receive Mark\'s booking info')
+    for booking_id in context.booking_ids:
+        booking_info = get_booking(booking_id)
+        if booking_info['firstname'] != context.user:
+            raise ValidationError(
+                "Incorrect user: {}".format(booking_info['firstname']))
