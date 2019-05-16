9c9c5547fec67fc642fc831315d8da919cf2ede5
Dragan Nikolic <dragan.d.nikolic@gmail.com>
2019-05-16 10:50:11 +0200

    start add booking test

--- 02_api_testing/features/restful_booking.feature
+++ 02_api_testing/features/restful_booking.feature
@@ -1,11 +1,16 @@
 Feature: Restful Booking Application
 
   Scenario: Test get booking
-    When I request Mark's booking
-    Then I should receive Mark's booking info
+    When I request Mark's bookings
+    Then I should receive Mark's bookings info
  
+  @wip @current
   Scenario: Test add booking
+    When I add new booking
+    Then I should see new booking in the list
 
+  @wip
   Scenario: Test update booking
 
+  @wip
   Scenario: Test remove booking    
--- 02_api_testing/steps/restful_booking_steps.py
+++ 02_api_testing/steps/restful_booking_steps.py
@@ -1,14 +1,23 @@
 from support.restful_booking_api import *
 
-@when(u'I request Mark\'s booking')
+@when(u'I request Mark\'s bookings')
 def step_impl(context):
     context.user = 'Mark'
     context.booking_ids = get_booking_ids(context.user)
 
-@then(u'I should receive Mark\'s booking info')
+@then(u'I should receive Mark\'s bookings info')
 def step_impl(context):
     for booking_id in context.booking_ids:
         booking_info = get_booking_by_id(booking_id)
         assert\
             booking_info['firstname'] == context.user,\
             "Got info for incorrect user: {}!".format(booking_info['firstname'])
+
+@when(u'I add new booking')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: When I add new booking')
+
+
+@then(u'I should see new booking in the list')
+def step_impl(context):
+    raise NotImplementedError(u'STEP: Then I should see new booking in the list')
